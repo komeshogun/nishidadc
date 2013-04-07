f801a7dca012e4e7d0f0c1ffd1b66349769c8304
yugolf <yugo.maede@gmail.com>
2013-04-07 14:25:37 +0900

    first commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,28 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Nishida Dental  Clinick"))
   }
-  
+
+  def index1 = Action {
+    Ok(views.html.index1("Nishida Dental  Clinick"))
+  }
+
+  def index2 = Action {
+    Ok(views.html.index2("Nishida Dental  Clinick"))
+  }
+
+  def index3 = Action {
+    Ok(views.html.index3("Nishida Dental  Clinick"))
+  }
+
+  def index4 = Action {
+    Ok(views.html.index4("Nishida Dental  Clinick"))
+  }
+
+  def index5 = Action {
+    Ok(views.html.index5("Nishida Dental  Clinick"))
+  }
 }
